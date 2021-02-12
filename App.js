9117421e41a9e045542e314bbd3e62ef8e0f30f4
Jeffersonn Lucas <jeffersonnlucas@gmail.com>
2021-02-12 14:04:41 -0300

    Trabalhando com botões

--- App.js
+++ App.js
@@ -2,13 +2,36 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nome: ""
+    };
+
+    this.getText = this.getText.bind(this);
+  }
+
+  getText(text) {
+    if (text.length > 0) {
+      this.setState({ nome: "Bem vindo: " + text });
+    } else {
+      this.setState({ nome: '' });
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
 
-        <TextInput style={styles.input} />
+        <TextInput
+          style={styles.input}
+          placeholder="Digite seu nome."
+          underlineColorAndroid='transparent'
+          onChangeText={this.getText} />
 
-        <Text></Text>
+        <Text style={styles.text}>
+          {this.state.nome}
+        </Text>
       </View>
     );
   }
--- README.md
+++ README.md
@@ -5,6 +5,11 @@
 # Inicializar um projeto
 npx react-native init nomeApp
 
+# Generate Android and IOS Folder
+yarn install 
+yarn add react-native-eject
+npx react-native eject
+
 #* Lembre-se sempre, antes de usar o comando para rodar o aplicativo precisa estar com o Emulador aberto e ai sim rodar o comando.
 
 # Rodar projeto no emulador Android
@@ -12,8 +17,4 @@ npx react-native run-android
 
 # Rodar projeto no emulador IOS
 npx react-native run-ios
-
-# Generate Android and IOS Folder 
-yarn add react-native-eject
-npx react-native eject
 ```
