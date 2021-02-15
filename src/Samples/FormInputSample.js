1e91743d1b7bafec34be83ca87f46f1f572c5012
Jeffersonn Lucas <jeffersonnlucas@gmail.com>
2021-02-15 20:02:12 -0300

    Rename Components to Sample

---
+++ src/Samples/FormInputSample.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+
+class FormInputComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            nome: "",
+            input: ""
+        };
+
+        this.signin = this.signin.bind(this);
+    }
+
+    signin() {
+        if (this.state.input == '') {
+            alert('Digite seu nome!');
+            return
+        }
+
+        this.setState({ nome: 'Bem vindo: ' + this.state.input })
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+
+                <TextInput
+                    style={styles.input}
+                    placeholder="Digite seu nome."
+                    underlineColorAndroid='transparent'
+                    onChangeText={(text) => this.setState({ input: text })} />
+
+                <Button title="Entrar" onPress={this.signin} />
+
+                <Text style={styles.text}>
+                    {this.state.nome}
+                </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    input: {
+        height: 45,
+        borderWidth: 1,
+        borderColor: '#222',
+        margin: 10,
+        fontSize: 20,
+        padding: 10
+    },
+    text: {
+        textAlign: 'center',
+        fontSize: 25
+    }
+});
+
+export default FormInputComponent;
